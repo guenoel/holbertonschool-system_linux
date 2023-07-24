70c44525d37c2d49791add3e4e26936db72b284c
guenoel <guenoel@gmail.com>
2023-07-24 18:46:49 +0200

    first test

---
+++ ls/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- fonction principale
+ * @argc: nb arguments
+ * @argv: array d arguments
+ *
+ * Return: 0 si tout va bien
+ */
+
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", (argc - 1));
+	for (; argv == 0;)
+		;
+	return (0);
+}
