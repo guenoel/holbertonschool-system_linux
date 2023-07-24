ba4f978b821876df0d66be9a7c15af8a72eb1904
guenoel <guenooel@hotmail.com>
2023-07-24 10:12:58 -0700

    first check

---
+++ ls/task0.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+/**
+ * main- fonction principale
+ * no arguments yet
+ *
+ * Return: 0 if all good 1 if errors
+ */
+
+
+int main(void)
+{
+	DIR *dir;
+	struct dirent *entry;
+	struct stat file_stat;
+
+	/**Open the current directory*/
+	dir = opendir(".");
+	if (dir == NULL)
+	{
+		perror("Error opening directory");
+		return (1);
+	}
+
+	/**Read the directory entries*/
+	while ((entry = readdir(dir)) != NULL)
+	{
+		if (lstat(entry->d_name, &file_stat) == -1)
+		{
+			perror("Error getting file status");
+			closedir(dir);
+			return (1);
+		}
+
+		/**Print file/directory name*/
+		printf(" %s\n", entry->d_name);
+	}
+
+	/**Close the directory*/
+	closedir(dir);
+	return (0);
+}
