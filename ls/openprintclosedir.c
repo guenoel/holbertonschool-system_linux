14cedfc432d53b5b936961fafc7fa2e3011a3ad9
guenoel <guenooel@hotmail.com>
2023-07-25 07:31:02 -0700

    change on error print

--- ls/openprintclosedir.c
+++ ls/openprintclosedir.c
@@ -8,36 +8,36 @@
 int openprintclosedir(char *path, char *prog_name)
 {
 	dir = opendir(path);
-        if (dir == NULL)
-        {
-		fprintf(stderr, "%s: No such file or directory\n", prog_name);
-                return (1);
-        }
+	if (dir == NULL)
+	{
+		fprintf(stderr, "%s: %s\n", prog_name, strerror(errno));
+		return (1);
+	}
 
 	printf("%s: ", path);
 
-        /**Read the directory entries*/
-        while ((entry = readdir(dir)) != NULL)
-        {
+	/**Read the directory entries*/
+	while ((entry = readdir(dir)) != NULL)
+	{
 		char full_path[1024];
 		sprintf(full_path, "%s/%s", path, entry->d_name);
 
-                if (lstat(full_path, &file_stat) == -1)
-                {
-			fprintf(stderr, "%s: error getting file status\n", prog_name);
-                        closedir(dir);
-                        return (1);
-                }
+		if (lstat(full_path, &file_stat) == -1)
+		{
+			fprintf(stderr, "%s: %s: error getting file status\n", prog_name, strerror(errno));
+			closedir(dir);
+			return (1);
+		}
 
-                /**Print file/directory name*/
-                if (entry->d_name[0] != '.')
-                {
-                printf("%s ", entry->d_name);
-                }
-        }
+		/**Print file/directory name*/
+		if (entry->d_name[0] != '.')
+		{
+			printf("%s ", entry->d_name);
+		}
+	}
 
-        /**Close the directory*/
-        closedir(dir);
+	/**Close the directory*/
+	closedir(dir);
 	return(0);
 }
 
