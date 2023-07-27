0abec893430cba7f3834353c570fc5bb185295b7
guenoel <guenooel@hotmail.com>
2023-07-27 13:41:20 -0700

    move files

---
+++ ls/openprintclosedir.c
@@ -0,0 +1,66 @@
+#include "hls.h"
+
+/**
+ * openprintclosedir - function that open, print and close directory
+ * @path: string that represent the path of the directory
+ */
+
+int openprintclosedir(char *path, char *prog_name, int nb, char *options)
+{
+	/**Read the directory entries*/
+
+	dir = opendir(path);
+	if (dir)
+	{
+		if (nb > 1)
+		{
+			printf("%s:\n", path);
+		}
+	} else {	
+		if (errno == EACCES)
+		{
+			fprintf(stderr, "%s: cannot open directory %s: ", prog_name, path);
+			perror("");
+			return (1);	
+		} else {
+			fprintf(stderr, "%s: cannot access %s: ", prog_name, path);
+			perror("");
+			return (1);
+		}
+	}
+	while ((entry = readdir(dir)) != NULL)
+	{
+		char full_path[1024];
+		sprintf(full_path, "%s/%s", path, entry->d_name);
+
+		if (lstat(full_path, &file_stat) == -1)
+		{
+			fprintf(stderr, "%s: Error getting file status %s: ", prog_name, path);
+			perror("");
+			closedir(dir);
+			return (1);
+		}
+
+		/**Print file/directory name*/
+		if (entry->d_name[0] != '.')
+		{
+			printf("%s", entry->d_name);
+			if (is_char_in_str(options, '1'))
+			{
+				printf("\n");
+			} else {
+				printf(" ");
+			}
+		}
+	}
+	if (!is_char_in_str(options, '1'))
+	{
+		printf("\n");
+	}
+
+	/**Close the directory*/
+	closedir(dir);
+
+	return(0);
+}
+
