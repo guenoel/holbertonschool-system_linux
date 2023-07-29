8c8d9baac7c698bfdad91043bcfa21e6512e0ad3
guenoel <guenooel@hotmail.com>
2023-07-29 16:11:51 -0700

    remove memset command

--- ls/init_struct_stat.c
+++ ls/init_struct_stat.c
@@ -11,6 +11,18 @@ void init_struct_stat(struct stat file_stat[1024])
 	int i = 0;
 
 	for (i = 0; i < 1024; i++) {
-		memset(&file_stat[i], 0, sizeof(struct stat));
+		file_stat[i].st_dev = 0;
+		file_stat[i].st_ino = 0;
+		file_stat[i].st_mode = 0;
+		file_stat[i].st_nlink = 0;
+		file_stat[i].st_uid = 0;
+		file_stat[i].st_gid = 0;
+		file_stat[i].st_rdev = 0;
+		file_stat[i].st_size = 0;
+		file_stat[i].st_blksize = 0;
+		file_stat[i].st_blocks = 0;
+		file_stat[i].st_atime = 0;
+		file_stat[i].st_mtime = 0;
+		file_stat[i].st_ctime = 0;
 	}
 }
