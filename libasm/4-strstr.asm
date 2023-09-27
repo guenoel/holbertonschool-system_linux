eb552247f5dda795a995aa42d1aebc95ac0b092b
guenoel <guenooel@hotmail.com>
2023-09-27 07:33:40 -0700

    return haystack if needle is empty

--- libasm/4-strstr.asm
+++ libasm/4-strstr.asm
@@ -11,9 +11,9 @@ asm_strstr:
     xor rdx, rdx       ; init rdx == dl (that will contain a letter of needle) to 0
     xor rbx, rbx       ; init rdx == dl (that will contain a letter of haystack) to 0
     cmp rsi, 0         ;
-    jz .not_found      ;
-    cmp rdi, 0         ;
     jz .empty_needle   ;
+    cmp rdi, 0         ;
+    jz .not_found      ;
 
 .init_needle:
     xor rcx, rcx       ; init rcx (that will contain loop counter) to 0
