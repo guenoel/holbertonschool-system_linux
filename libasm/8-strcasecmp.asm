c548c65cf4fdd91a168c9c8aafb6c5be30496080
guenoel <guenooel@hotmail.com>
2023-09-28 09:01:41 -0700

    add comments

--- libasm/8-strcasecmp.asm
+++ libasm/8-strcasecmp.asm
@@ -10,26 +10,27 @@ asm_strcasecmp:
     xor rax, rax    ; init rax (that contain return value) to 0
 
 .loop:
+;.fst_str_check:
     mov al, byte [rdi] ; Load the byte at the address pointed to by rdi in al
-    cmp al, 'A'        ;
-    jl .sec_str_check  ;
-    cmp al, 'Z'        ;
-    jg .sec_str_check  ;
-    add al, 32         ;
+    cmp al, 'A'        ; compare letter of first string with first uppercase alphabet letter
+    jl .sec_str_check  ; if lower it's not a uppercase letter
+    cmp al, 'Z'        ; compare letter of first string with last uppercase alphabet letter
+    jg .sec_str_check  ; if higher it's not a uppercase letter
+    add al, 32         ; if not uppercase letter convert to uppercase (or int + 32 if not a letter)
 
 .sec_str_check:
-	mov bl, byte [rsi] ; Load the byte at the address pointed to by rsi in bl
-    cmp bl, 'A'        ;
+	mov bl, byte [rsi] ; same comment as previous reference for second string
+    cmp bl, 'A'        ; 
     jl .compare_chars  ;
     cmp bl, 'Z'        ;
     jg .compare_chars  ;
     add bl, 32         ;
 
 .compare_chars:
-    sub bl, al         ;
-    cmp bl, 0          ;
+    sub bl, al         ; bl = bl - al
+    cmp bl, 0          ; result stored in bl must be 0 if equal
     je .equal          ; if result is equal jump equal
-    jmp .end
+    jmp .end           ; else if different jump to end
 
 .equal:
     cmp al, 0          ; check if end of first string
@@ -39,7 +40,8 @@ asm_strcasecmp:
     jmp .loop          ; back to loop
 
 .end:
-    mov al, bl         ;
+    mov al, bl         ; copy result of sub (line 29) in al (return value registry)
+
     mov rsp, rbp       ; move base pointer to top of stack
     pop rbp            ; free rbp pointer
     ret                ; return value of rax
