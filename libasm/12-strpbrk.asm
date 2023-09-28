d0c1e501aa77eeb5e98cca49a4302803d0c7377c
guenoel <guenooel@hotmail.com>
2023-09-28 15:30:13 -0700

    add comments

--- libasm/12-strpbrk.asm
+++ libasm/12-strpbrk.asm
@@ -15,28 +15,28 @@ asm_strpbrk:
 .main_loop:
     mov al, byte [rdi + rcx] ; Load the byte at the address pointed to by rdi in al
     cmp al, 0          ; check if end of first string
-    je .not_found            ; if NULL character go to end
+    je .not_found      ; if NULL character go to not_found
 
 .sec_loop:
 	mov bl, byte [rsi + rdx] ; Load the byte at the address pointed to by rsi in bl
     inc rdx            ; increment sec_loop counter
     cmp bl, 0          ; check if end of second string
-    je .next_letter            ; if NULL character go to end
+    je .next_letter    ; if NULL character go to next_letter of first string
     cmp al, bl         ; compare values of char of first str and char of sec str
-    je .found          ; if result is equal jump equal
+    je .found          ; if result is equal jump .found
     jmp .sec_loop      ; else jump back to sec_loop
 
 .next_letter:
-    xor rdx, rdx
-    inc rcx
-    jmp .main_loop
+    xor rdx, rdx       ; reset counter of second string to 0
+    inc rcx            ; increment counter of first string
+    jmp .main_loop     ; jump to .main_loop
 
 .not_found:
-    xor rax, rax
-    jmp .end
+    xor rax, rax       ; set return value to 0
+    jmp .end           ; go to end
 
 .found:
-    lea rax, [rdi + rcx]
+    lea rax, [rdi + rcx]; set memory address of the found letter in first string
 
 .end:
     mov rsp, rbp       ; move base pointer to top of stack
