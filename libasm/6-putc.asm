0384b8f91c5f6c321d0f57b79dddb02ad7067b2c
guenoel <guenooel@hotmail.com>
2023-09-28 02:03:47 -0700

    task 6

--- libasm/6-putc.asm
+++ libasm/6-putc.asm
@@ -7,9 +7,9 @@ asm_putc:
     push rbp             ; save base pointer before mov
     mov rbp, rsp         ; move top of stack to base pointer
     xor rax, rax         ; init rax (that will contain return value) to 0
-    
+
     mov rax, 1           ; syscall number for write (in x86-64 on Linux)
-    
+
     ; Arguments for the write syscall - write (int fd, void* buf, size_t cnt) buf -> rsi ?
     mov [rsi], rdi       ; Places the character (passed in RDI) in the memory address pointed to by RSI (buf argument of write)
     mov rdi, 1           ; Standard Output File Descriptor
