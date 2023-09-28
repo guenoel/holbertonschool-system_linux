bb4c95b7d7c9e3f54ec2252d41de2b57810fb6a3
guenoel <guenooel@hotmail.com>
2023-09-28 05:29:50 -0700

    task 7

--- libasm/7-puts.asm
+++ libasm/7-puts.asm
@@ -5,21 +5,23 @@ global asm_puts
     section .text
 
 asm_puts:
-    push rbp              ; save base pointer before mov
-    mov rbp, rsp          ; move top of stack to base pointer
+    push rbp           ; save base pointer before mov
+    mov rbp, rsp       ; move top of stack to base pointer
 
-    call asm_strlen       ; call extern function
-    mov rdx, rax          ; save len (stored in rax by strlen) in rdx
+    push rdi           ; save rdi to stack
 
-    mov rax, 1            ; syscall number for write (in x86-64 on Linux)
-    mov rdi, 1            ; Standard Output File Descriptor
-    mov rsi, [rdi]        ; put string from rdi to rsi (buffer)
+    call asm_strlen    ; call extern function
+    mov rdx, rax       ; save len (stored in rax by strlen) in rdx
+
+    mov rax, 1         ; syscall number for write (in x86-64 on Linux)
+    mov rdi, 1         ; Standard Output File Descriptor
+    mov rsi, [rsp]     ; put string from rsp (stack) to rsi (buffer)
     ; rdx set at line 12
 
-    syscall               ; Call the write syscall to print the character
+    syscall            ; Call the write syscall to print the character
 
-    mov rax, rdx          ; Value of rdx (original length) back to rax (return value of puts)
-
+    mov rax, rdx       ; Value of rdx (original length) back to rax (return value of puts)
+    pop rdi            ; remove rdi from stack
     mov rsp, rbp       ; move base pointer to top of stack
     pop rbp            ; free rbp pointer
     ret                ; return value of rax
