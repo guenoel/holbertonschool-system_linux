2cbe1b9e216fe6725c354ad98b3207004491edac
guenoel <guenooel@hotmail.com>
2023-09-28 13:36:39 -0700

    add comments

--- libasm/10-strspn.asm
+++ libasm/10-strspn.asm
@@ -13,24 +13,24 @@ asm_strspn:
 .main_loop:
     mov al, byte [rdi + rcx] ; Load the byte at the address pointed to by rdi in al
     cmp al, 0          ; check if end of first string
-    je .end            ; if equal go to str1_end
+    je .end            ; if NULL character go to end
 
 .sec_loop:
 	mov bl, byte [rsi + rdx] ; Load the byte at the address pointed to by rsi in bl
-    inc rdx            ;
+    inc rdx            ; increment sec_loop counter
     cmp bl, 0          ; check if end of second string
-    je .end           ; if equal go to str2_end
-    cmp al, bl         ; compare those 2 values
+    je .end            ; if NULL character go to end
+    cmp al, bl         ; compare values of char of first str and char of sec str
     je .equal          ; if result is equal jump equal
-    jmp .sec_loop
+    jmp .sec_loop      ; else jump back to sec_loop
 
 .equal:
-    inc rcx            ; increment loop counter
+    inc rcx            ; increment main_loop counter that will be returned at the end
     xor rdx, rdx       ; reset sec_loop counter to 0
     jmp .main_loop     ; back to main_loop
 
 .end:
-    mov rax, rcx       ; copy counter in return register
+    mov rax, rcx       ; copy main_loop counter in return register
     mov rsp, rbp       ; move base pointer to top of stack
     pop rbp            ; free rbp pointer
     ret                ; return value of rax
