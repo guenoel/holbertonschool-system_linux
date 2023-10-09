f758bc04f9b70a7076a2a9e053cdb437776cfbb9
guenoel <guenooel@hotmail.com>
2023-10-09 06:36:15 -0700

    bettyfy code

--- signals/0-handle_signal.c
+++ signals/0-handle_signal.c
@@ -1,14 +1,18 @@
 #include "signals.h"
 
+/**
+ * handle_signal - catch signal and execute function with signal number in parameter
+ * Return: 0 if ok -1 if error
+*/
 int handle_signal(void)
 {
-    if (signal(SIGINT, sigint_handler) == SIG_ERR)
-        return (-1);
-    return (0);
+	if (signal(SIGINT, sigint_handler) == SIG_ERR)
+		return (-1);
+	return (0);
 }
 
 void sigint_handler(int signum)
 {
-    printf("Gotcha! [%d]\n", signum);
-    fflush(stdout);
+	printf("Gotcha! [%d]\n", signum);
+	fflush(stdout);
 }
