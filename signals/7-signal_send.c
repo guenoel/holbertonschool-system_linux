d18ef474f9e7b4e56a120e959414221b38d03006
guenoel <guenooel@hotmail.com>
2023-10-16 12:16:00 -0700

    task 7

---
+++ signals/7-signal_send.c
@@ -0,0 +1,36 @@
+#include "signals.h"
+
+/**
+* main - send signal to a pid
+*
+* @argc: number of arguments
+* @argv: array of strings arguments
+*
+* Return: 1 if OK 0 if failed
+*/
+int main(int argc, char *argv[])
+{
+	int pid_num;
+
+	if (argc != 2)
+	{
+		printf("Usage: %s <PID>\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+
+	pid_num = atoi(argv[1]);
+
+	if (pid_num == 0)
+	{
+		printf("Usage: %s <PID>\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+
+	if (kill(pid_num, SIGINT) == -1)
+	{
+		perror("kill");
+		return (EXIT_FAILURE);
+	}
+
+	return (EXIT_SUCCESS);
+}
