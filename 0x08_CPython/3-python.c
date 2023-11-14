bb1b905166d1b43277924179040f3273d5259029
guenoel <guenooel@hotmail.com>
2023-11-14 01:59:53 -0800

    move \'[*] Python list info\'

--- 0x08_CPython/3-python.c
+++ 0x08_CPython/3-python.c
@@ -15,6 +15,7 @@ void print_python_float(PyObject *p);
 */
 void print_python_list(PyObject *p)
 {
+	printf("[*] Python list info!\n");
 	/* Check if the object is a valid List object */
 	if (!PyList_Check(p))
 	{
@@ -36,7 +37,6 @@ void print_python_list(PyObject *p)
 	alloc = ((PyListObject *)p)->allocated;
 
 	/* Print information about the Python List */
-	printf("[*] Python list info!\n");
 	printf("[*] Size of the Python List = %zd\n", size);
 	printf("[*] Allocated = %zd\n", alloc);
 
